14ac48c4bcf29fd3ebd4b6d608882869bad9647d
khalilb2000 <khalilb2000@gmail.com>
2023-11-12 19:32:26 -0600

    adding logic to putDb

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,6 +14,8 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+const txtEditorDB = await openDB('jate', 1);
+const transVar = txtEditorDB.transaction('jate', 'readwrite');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
